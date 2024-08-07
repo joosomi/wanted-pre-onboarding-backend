b7183f3d581882995893456280aa2aa61074d0d4
joosomi <wnthal1211@gmail.com>
2024-08-07 16:44:07 +0900

    feat: 채용 공고 수정 PUT 요청

--- controllers/Cjob.js
+++ controllers/Cjob.js
@@ -16,6 +16,29 @@ exports.createJob = async (req, res) => {
     }
 };
 
+exports.updateJob = async (req, res) => {
+    try {
+        const { position, reward, description, skills } = req.body;
+        const job = await Job.findByPk(req.params.id);
+
+        if (!job) {
+            return res.status(404).json({ error: 'Job not found' });
+        }
+
+        // 채용 공고에서 수정된 값이 있을 경우 수정
+        if (position !== undefined) job.position = position;
+        if (reward !== undefined) job.reward = reward;
+        if (description !== undefined) job.description = description;
+        if (skills !== undefined) job.skills = skills;
+
+        await job.save();
+
+        res.status(200).json(job);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
 // exports.getJobs = async (req, res) => {
 //     try {
 //         const jobs = await Job.findAll({
--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -5,6 +5,9 @@ const Cjob = require('../controllers/Cjob');
 //채용 공고 생성
 router.post('/', Cjob.createJob);
 
+// 채용 공고 수정
+router.put('/:id', Cjob.updateJob);
+
 // // 모든 채용 공고 조회
 // router.get('/', Cjob.getJobs);
 
